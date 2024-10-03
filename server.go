64900d04eef9f6c1457773c72972083c25f5a44b
Pierre Garamendi <pierre.garamendi@gmail.com>
2024-10-03 10:01:50 -0500

    message from server added

--- server.go
+++ server.go
@@ -3,10 +3,12 @@ package main
 import (
 	"capsynth/middleware"
 	"capsynth/subtitles"
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/subtitles", middleware.ContentTypeMiddleware((subtitles.WelcomeController)))
+	fmt.Println("Server is running on port 8080...")
 	http.ListenAndServe(":8080", nil)
 }
